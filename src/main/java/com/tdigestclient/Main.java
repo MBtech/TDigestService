703ec121e9fc9c19711032e549a545d1207bfc3d
MB <mbilal_ce@live.com>
2016-07-22 18:52:46 +0200

    percentile arg

--- src/main/java/com/tdigestclient/Main.java
+++ src/main/java/com/tdigestclient/Main.java
@@ -10,8 +10,7 @@ public class Main {
     public static void main(String[] args){
         TDigestClient tc =  new TDigestClient(args[0],Integer.parseInt(args[1]));
         tc.connection();
-//        tc.addElement(10.0);
-        tc.getPercentile(0.90);
+        tc.getPercentile(Double.parseDouble(args[2]));
         tc.quitConnection();
     }
 }
